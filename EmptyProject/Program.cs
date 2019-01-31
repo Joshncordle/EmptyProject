99a347fe88d7ca5df0b9bd76be83b2a3801efd7f
Josh Cordle <joshncordle@gmail.com>
2019-01-31 17:57:42 -0500

    Version 1

--- EmptyProject/Program.cs
+++ EmptyProject/Program.cs
@@ -6,7 +6,19 @@ namespace EmptyProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name...");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int user_input = int.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("Please only enter your name");
+                Console.WriteLine("Press any key to exit the program");
+                Console.ReadKey(true);
+            }
         }
     }
 }
